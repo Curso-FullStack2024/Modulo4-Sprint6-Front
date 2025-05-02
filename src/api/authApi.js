f78cc6051b21efc7f2df639572d139c6c09cfca7
Alejandro Luna <alejandro1672@gmail.com>
2025-05-02 16:10:18 -0300

    add url API

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 const api=axios.create({
-    baseURL:'http://127.0.0.1:3500',
+    //baseURL:'http://127.0.0.1:3500',
+    baseURL:'https://modulo4-sprint6-back-1.onrender.com'
 })
 
 export const createUsuario =  (user) =>  api.post('/auth/register', user)
